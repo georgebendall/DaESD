d639b35005ba713ae70d2fc5afa7ea7f7f30f817
Fawzi <fawzi@fawzis-MacBook-Air-2.local>
2026-03-17 18:50:02 +0000

    Added homepage and improved navbar UI

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
-
+from django.views.generic import TemplateView
 urlpatterns = [
     path("accounts/", include("accounts.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
     path("", include("dashboards.urls")),
     path("orders/", include("orders.urls")),
     path("shop/", include("catalog.urls")),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
